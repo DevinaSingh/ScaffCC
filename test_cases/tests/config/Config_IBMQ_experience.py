30aeff4b63a5e3b8b342d55113114b2ea98b4f67
Devina Singh <devinas@princeton.edu>
2019-09-09 02:01:30 -0700

    Updating Clang and LLVM to V.8.0.1

---
+++ test_cases/tests/config/Config_IBMQ_experience.py
@@ -0,0 +1,2 @@
+API_token = '988b8de91ccfaf82749dc8f02c31a79116a5b7c4da4dafe103bfbbec931f21b61abe48a61d171a28328058d95a224a9850ef90df06e0ee6f999446ee46828ead'
+API_URL = 'https://api.quantum-computing.ibm.com/api/Hubs/ibm-q/Groups/open/Projects/main'
